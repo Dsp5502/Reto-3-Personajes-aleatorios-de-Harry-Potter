f628abe52ddde8df2d49ecb849ec7cfbb021edef
David Puerto Guerrero <dsp5502@gmail.com>
2022-02-16 15:16:46 -0500

    Agregar correcion al estilo aleatorio

--- script/script.js
+++ script/script.js
@@ -1,22 +1,27 @@
 import { data } from '../data/data.js';
 
-let personajes = document.getElementById('personajes');
+const element = document.getElementById('boton');
+element.addEventListener('click', botonAleatorio);
 
-personajes.innerHTML = '';
+function botonAleatorio() {
+  let random = Math.floor(Math.random() * data.length);
 
-let random = Math.floor(Math.random() * data.length);
+  let personaje = data.find((perso) => perso.id == Number(random));
 
-let personaje = data.find((perso) => perso.id == Number(random));
-
-personajes.innerHTML += `
+  document.getElementById('personajes').innerHTML = `
   <div class="fondoTextura" >
 
   <div class="fondoCard" >
   <img class="foto" src="${personaje.imagen}" alt="personas">
   <li class=" letra" > ${personaje.personaje}</li>
   <li class=" letra" >${personaje.casa}</li>
-  <button class="boton2">Nuevo Personaje</button>
+  <button id="botonnuevo" class="boton2">Nuevo Personaje</button>
   
   </div>
   </div>
   `;
+
+  document
+    .getElementById('botonnuevo')
+    .addEventListener('click', botonAleatorio);
+}
